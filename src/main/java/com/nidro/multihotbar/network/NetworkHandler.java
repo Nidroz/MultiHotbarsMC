8208e7f2935e3a9bf2e814c34b9da7ba06d2334b
Nadir Sidhoum <na.sidhoum@gmail.com>
2026-05-06 22:23:04 +0200

    NETWORK HANDLER (add network handler and register the packet type and handler

--- src/main/java/com/nidro/multihotbar/network/NetworkHandler.java
+++ src/main/java/com/nidro/multihotbar/network/NetworkHandler.java
@@ -1,4 +1,20 @@
 package com.nidro.multihotbar.network;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
 public class NetworkHandler {
+  private static void onRegisterPayloads(RegisterPayloadHandlersEvent event) {
+    PayloadRegistrar registrar = event.registrar("1");  // "1" is the channel name, can be anything but should be unique to avoid conflicts with other mods
+    registrar.playToServer(
+            SwapHotbarPayload.TYPE,
+            SwapHotbarPayload.STREAM_CODEC,
+            SwapHotbarPayload::handle
+    );
+  }
+
+  public void register(IEventBus modEventBus) {
+    modEventBus.addListener(NetworkHandler::onRegisterPayloads);
+  }
 }
