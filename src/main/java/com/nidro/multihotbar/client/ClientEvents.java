4228927303dffb72a300a9c9d9043d4c06b63dab
Nadir Sidhoum <na.sidhoum@gmail.com>
2026-05-06 22:15:56 +0200

    CLIENT EVENTS (next & prev hotbar)

---
+++ src/main/java/com/nidro/multihotbar/client/ClientEvents.java
@@ -0,0 +1,23 @@
+package com.nidro.multihotbar.client;
+
+import com.nidro.multihotbar.network.SwapHotbarPayload;
+import net.minecraft.client.Minecraft;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+public class ClientEvents {
+  @SubscribeEvent
+  public static void onClientTick(ClientTickEvent.Post event) {
+    Minecraft mc = Minecraft.getInstance();
+    // ignore inputs when no player or a screen is open
+    if (mc.player == null || mc.screen != null) return;
+
+    while (KeyBindings.NEXT_HOTBAR.consumeClick()) {
+      PacketDistributor.sendToServer(new SwapHotbarPayload(1));
+    }
+    while (KeyBindings.PREV_HOTBAR.consumeClick()) {
+      PacketDistributor.sendToServer(new SwapHotbarPayload(-1));
+    }
+  }
+}
