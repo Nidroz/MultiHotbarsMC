35b5ed3769aca719a0ce69d3409d9c914d08df8d
Nadir Sidhoum <na.sidhoum@gmail.com>
2026-05-06 22:18:17 +0200

    ClientEvents (@EventBusSubscriber add)

--- src/main/java/com/nidro/multihotbar/client/ClientEvents.java
+++ src/main/java/com/nidro/multihotbar/client/ClientEvents.java
@@ -1,11 +1,15 @@
 package com.nidro.multihotbar.client;
 
+import com.nidro.multihotbar.MultiHotbarMod;
 import com.nidro.multihotbar.network.SwapHotbarPayload;
 import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
+@EventBusSubscriber(modid = MultiHotbarMod.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
   @SubscribeEvent
   public static void onClientTick(ClientTickEvent.Post event) {
